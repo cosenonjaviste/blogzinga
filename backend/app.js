2f775e6f31cc6ec9f1d6318b23723539e05a0100
whisher <whisher06@gmail.com>
2015-02-20 11:10:17 +0100

    ilwebdifabio.it by whisher

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+console.log(path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /// catch 404 and forward to error handler
