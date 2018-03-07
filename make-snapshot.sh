128857bac34e5d24aa6e1eb70d9ebab143d9fcc5
CoseNonJaviste GitHub <cosenonjaviste@users.noreply.github.com>
2018-03-07 22:17:32 +0100

    Fixed script

--- make-snapshot.sh
+++ make-snapshot.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-U=$1
-#P=$( echo "$U" | perl -MURI -le 'chomp($url = <>); print URI->new($url)->fragment' )
-final_url=$U
-P=${P:1}
+url=$1
 final_path=$2
 base_path=./dist/snapshots
 mkdir -p $base_path
-phantomjs ./bower_components/AngularJS-SEO-Article/.phantomjs-runner.js $final_url > $base_path/$final_path
+phantomjs ./bower_components/AngularJS-SEO-Article/.phantomjs-runner.js $url > $base_path/$final_path
